f9382f6da340251a47f0bd76fdfaf493234989b4
Georgi Mirazchiyski <georgimweb@gmail.com>
2017-03-29 22:43:10 +0100

    Update PlatformMovement.cs

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -1,32 +1,35 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformMovement : MonoBehaviour {
-
+public class PlatformMovement : MonoBehaviour 
+{
     private GameObject platform;
     public float moveSpeed;
     public Transform currentPoint;
     public Transform[] points;
     public int pointSelection;
-	// Use this for initialization
-	void Start () {
-        platform = this.gameObject;
-        currentPoint = points[pointSelection];
-	}
+    
+    // Use this for initialization
+    void Start ()
+    {
+    	platform = this.gameObject;
+	currentPoint = points[pointSelection];
+    }
 	
-	// Update is called once per frame
-	void Update () {
-        platform.GetComponent<Transform>().position = Vector3.MoveTowards(platform.transform.position, currentPoint.position, Time.deltaTime * moveSpeed);
-        if(platform.transform.position== currentPoint.position)
-        {
-            pointSelection++;
-            if (pointSelection == points.Length)
+    // Update is called once per frame
+    void Update ()
+    {
+    	platform.GetComponent<Transform>().position = Vector3.MoveTowards(platform.transform.position, currentPoint.position, Time.deltaTime * moveSpeed);
+	if(platform.transform.position == currentPoint.position)
+	{
+	    pointSelection++;
+	    if (pointSelection == points.Length)
             {
-                pointSelection = 0;
+	        pointSelection = 0;
             }
-        }
-        
-        currentPoint = points[pointSelection];
+	}
+
+	currentPoint = points[pointSelection];
     }
 }
